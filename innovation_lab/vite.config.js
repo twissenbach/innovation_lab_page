9dc1071aec4b2915c15b81049d2c1cf5289350b7
Troy Wissenbach <a02312731@usu.edu>
2025-02-20 09:52:47 -0700

    changing import

--- innovation_lab/vite.config.js
+++ innovation_lab/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  server: {
+    port: process.env.PORT,
+  },
+  plugins: [react()],
+});
