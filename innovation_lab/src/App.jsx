9fb6d6e66981ec87780c772abe10449777bbdee1
Troy Wissenbach <a02312731@usu.edu>
2025-02-20 09:33:01 -0700

    static page with link

---
+++ innovation_lab/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="header">
+        <h1>ASC Innovation Lab</h1>
+        <h2>Building Full-Stack Applications Using AI</h2>
+      </div>
+      <div className="content">
+        <a href="https://docs.google.com/document/d/1wFrquWQeRGOEDl14VnSuDNfpPc_zjJ3u6aIkuwhLu7M/edit?usp=sharing" target="_blank" rel="noopener noreferrer">
+          Software Setup
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default App
